341a64000d48e7fe0ebc27b085de2abeb793caee
Arman Hossain <hossain4arman@gmail.com>
2026-02-11 20:37:09 +0600

    All lessons Display Added: add loading and empty states, and implement word pronunciation feature

--- Module-05/english-janala/script.js
+++ Module-05/english-janala/script.js
@@ -9,7 +9,50 @@ const levelLessons = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url) 
         .then(id => id.json())
-        .then(idData => console.log(idData)) ;
+        .then(idData => displaylessons(idData.data)) ;
+}
+
+function wordSpeak(word){
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = "en-EN"; // English
+     window.speechSynthesis.speak(utterance);
+}
+
+const displaylessons = (lessons) => {
+    const lessonContainer = document.getElementById('lessonCard') ;
+    const LoadingContainer = document.getElementById('loadingCard');
+    const EmptryContainer = document.getElementById('emptyCard') ;
+
+    EmptryContainer.classList.add('hidden');
+    LoadingContainer.classList.add('hidden');
+    lessonContainer.classList.add('grid') ;
+    lessonContainer.classList.remove('hidden') ;
+
+    lessonContainer.innerHTML="" ;
+
+    if (lessons.length == 0 || !lessons) {
+        EmptryContainer.classList.remove('hidden');
+    }
+
+    for(let lesson of lessons){  
+        const cardDiv = document.createElement('div') ;
+        cardDiv.className ="flex flex-col justify-center items-center gap-2 text-center p-5 bg-white rounded-md"
+        cardDiv.innerHTML=
+        `
+            <h1 class="font-bold text-2xl">${lesson.word}</h1>
+            <h2 class="font-medium text-ld">Meaning/Pronounciation</h2>
+            <h1 class="font-semibold hind-siliguri-regular ">"${lesson.meaning}/ ${lesson.pronunciation}"</h1>
+            <div class="flex justify-between items-center w-full pt-10 px-4">
+              <button class="p-2 rounded-sm bg-slate-200 w-7"><img src="assets/fi-sr-info.png" alt="information"></button>
+              <button onClick="wordSpeak(${lesson.word})"
+              class="p-2 rounded-sm bg-slate-200 w-7"><img src="assets/fi-sr-volume.png" alt="information"></button>
+            </div>
+
+        `
+
+        lessonContainer.appendChild(cardDiv) ;
+    }
+
 }
 
 const displaylevels = (datas) => {
