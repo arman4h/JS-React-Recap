d61062b18d60c290295eccd7059273b31409b890
Arman Hossain <hossain4arman@gmail.com>
2026-03-01 21:26:04 +0600

    Practicing Module 8

---
+++ Module-8/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+        <h1>Arman Hossain</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
